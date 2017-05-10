6a261fc8f8ec4bd0a8d7477f33d5db08bef429b7
mateus <mateus@ativarsoft.com>
2017-05-10 10:27:20 -0300

    added post functionality to rest program

--- rest.c
+++ rest.c
@@ -8,19 +8,51 @@
 #include <string.h>
 #include <ctype.h>
 #include <libintl.h>
+#include <curl/curl.h>
 
 #define _ gettext
 
 static const char copyright[] = "Copyright (C) 2017 Mateus de Lima Oliveira";
 
+char *url = NULL;
+int post = 0;
+
+int do_post()
+{
+	CURL *curl;
+	CURLcode res;
+
+	curl_global_init(CURL_GLOBAL_ALL);
+	curl = curl_easy_init();
+	if (!curl)
+		return 1;
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	if (post) {
+		/* Use read callback function instead of string. */
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, NULL);
+		/* Read from FILE * pointed by CURLOPT_READDATA. */
+		curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
+		/* Leave stdin as file descriptor */
+		curl_easy_setopt(curl, CURLOPT_READDATA, NULL);
+	}
+	res = curl_easy_perform(curl);
+	if(res != CURLE_OK)
+		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+		curl_easy_strerror(res));
+	curl_global_cleanup();
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	int key_size = -1, value_size = -1;
 	int c;
-	char *url;
 	
-	while ((c = getopt(argc, argv, "e:d:k:v:")) != -1) {
+	while ((c = getopt(argc, argv, "pu:d:k:v:")) != -1) {
 		switch (c) {
+			case 'p':
+			post = 1;
+			break;
 			case 'u':
 			url = optarg;
 			break;
@@ -46,9 +78,12 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	if (key_size <= 0) {
-		fprintf(stderr, "%s: %s\n", argv[0], _("key size not specified"));
+	if (!url) {
+		fprintf(stderr, "url not specified.\n");
 		return 1;
 	}
+
+	do_post();
+
 	return 0;
 }
