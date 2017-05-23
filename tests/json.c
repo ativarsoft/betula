49c0de4a64b801f78e9284f53db62d4cd2e57446
mateus <mateus@ativarsoft.com>
2017-05-23 14:25:31 -0300

    add json

--- tests/json.c
+++ tests/json.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
-#include <templatizer/json.h>
+#include <string.h>
+#include <stddef.h>
+#include <templatizer/templatizer.h>
 
-const char data[] = "{\"number\": 1234.5678}";
+const char text[] = "{\"number\": 1}";
+struct data {
+	double x;
+} bin;
+const struct tmpl_json_member members[] = {
+	{TMPL_JSON_NUMBER, "number", offsetof(struct data, x)}
+};
+struct tmpl_json_object root = {
+	(struct tmpl_json_member *) members,
+	&bin
+};
 
 int main()
 {
 	struct tmpl_json_context *context;
 
-	context = tmpl_context_create();
+	context = tmpl_json_create();
 	if (context == NULL)
 		return 1;
-	tmpl_json_feed(context, strlen(data), data);
+	tmpl_json_feed(context, strlen(text), text);
 	tmpl_json_eof(context);
-	tmpl_context_destroy(context);
+	tmpl_json_destroy(context);
 	return 0;
 }
