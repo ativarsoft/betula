3aa7d7009da489438a7661e8df019e7a536cd50a
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-02-26 18:56:37 -0300

    Improved helloworld plugin
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- plugins/input/helloworld.c
+++ plugins/input/helloworld.c
@@ -1,6 +1,8 @@
-#include "templatizer.h"
+/* Copyright (C) 2023 Mateus de Lima Oliveira */
 
-static int init(struct context *data, struct templatizer_callbacks *cb)
+#include <templatizer.h>
+
+static int init(tmpl_ctx_t data, tmpl_cb_t cb)
 {
 	cb->send_default_headers(data);
 	cb->add_filler_text(data, "Hello world!");
