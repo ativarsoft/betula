c0c6ec7422671c5a34d638d42d25ebd628b81a61
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-02-28 10:19:10 -0300

    Improved JIT with code for arithmetic
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- plugins/codegen/llvm.c
+++ plugins/codegen/llvm.c
@@ -1,15 +1,17 @@
 #include <templatizer.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <threads.h>
 #include <llvm-c/Core.h>
 #include <llvm-c/ExecutionEngine.h>
 #include <llvm-c/Analysis.h>
 #include <llvm-c/Target.h>
 #include <llvm-c/BitWriter.h>
 
-LLVMModuleRef mod;
-LLVMBuilderRef builder;
-LLVMValueRef current_fn;
+thread_local LLVMModuleRef mod;
+thread_local LLVMBuilderRef builder;
+thread_local LLVMValueRef current_fn;
+thread_local LLVMValueRef last_expr;
 
 int pollen_codegen_sanity_check()
 {
@@ -57,18 +59,151 @@ int pollen_codegen_sanity_check()
     return 0;
 }
 
-int gen_add_node()
+static int gen_add_node_start(tmpl_ctx_t ctx, const char *el, const char **attr)
 {
-    LLVMValueRef x = LLVMGetParam(current_fn, 0);
-    LLVMValueRef y = LLVMGetParam(current_fn, 1);
+    const char *x_str = NULL;
+    const char *y_str = NULL;
+    for (int i = 0; attr[i] != NULL; i += 2) {
+        const char *name = attr[i];
+        const char *value = attr[i+1];
+        if (strcmp(name, "x") == 0) {
+            x_str = value;
+        } else if (strcmp(name, "y") == 0) {
+            y_str = value;
+        }
+    }
+
+    if (x_str == NULL || y_str == NULL) {
+        fprintf(stderr, "error: x or y attribute missing from add element\n");
+        return -1;
+    }
+
+    int x = atoi(x_str);
+    int y = atoi(y_str);
     LLVMValueRef sum = LLVMBuildAdd(builder, x, y, "sum");
     LLVMBuildRet(builder, sum);
     return 0;
 }
 
+static int gen_add_node_end(tmpl_ctx_t ctx, const char *el)
+{
+    return 0;
+}
+
+static int gen_sub_node_start(tmpl_ctx_t ctx, const char *el, const char **attr)
+{
+    const char *x_str = NULL;
+    const char *y_str = NULL;
+    for (int i = 0; attr[i] != NULL; i += 2) {
+        const char *name = attr[i];
+        const char *value = attr[i+1];
+        if (strcmp(name, "x") == 0) {
+            x_str = value;
+        } else if (strcmp(name, "y") == 0) {
+            y_str = value;
+        }
+    }
+
+    if (x_str == NULL || y_str == NULL) {
+        fprintf(stderr, "error: x or y attribute missing from sub element\n");
+        return -1;
+    }
+
+    int x = atoi(x_str);
+    int y = atoi(y_str);
+    LLVMValueRef diff = LLVMBuildSub(builder, x, y, "diff");
+    LLVMBuildRet(builder, diff);
+    return 0;
+}
+
+static int gen_sub_node_end(tmpl_ctx_t ctx, const char *el)
+{
+    return 0;
+}
+
+static int gen_mul_node_start(tmpl_ctx_t ctx, const char *el, const char **attr)
+{
+    const char *x_str = NULL;
+    const char *y_str = NULL;
+    for (int i = 0; attr[i] != NULL; i += 2) {
+        const char *name = attr[i];
+        const char *value = attr[i+1];
+        if (strcmp(name, "x") == 0) {
+            x_str = value;
+        } else if (strcmp(name, "y") == 0) {
+            y_str = value;
+        }
+    }
+
+    if (x_str == NULL && y_str == NULL) {
+        fprintf(stderr, "error: x and y attributes missing from mul element\n");
+        return -1;
+    } else if (x_str == NULL) {
+    	fprintf(stderr, "error: x attribute missing from mul element\n");
+        return -1;
+    } else if (y_str == NULL) {
+        int x = atoi(x_str);
+        LLVMValueRef last = LLVMBuildLoad(builder, last_expr, "x_val");
+        LLVMValueRef c = LLVMConstInt(LLVMTypeOf(last_expr), atoi(x_str), 1);
+        LLVMValueRef prod = LLVMBuildMul(builder, last, c, "prod");
+        last_expr = prod;
+    } else if (x_str != NULL && y_str != NULL) {
+    	int x = atoi(x_str);
+        int y = atoi(y_str);
+        LLVMValueRef prod = LLVMBuildMul(builder, x, y, "prod");
+        last_expr = prod;
+    } else {
+    	abort();
+    }
+    return 0;
+}
+
+static int gen_mul_node_end(tmpl_ctx_t ctx, const char *el)
+{
+    return 0;
+}
+
+static int gen_div_node_start(tmpl_ctx_t ctx, const char *el, const char **attr)
+{
+    const char *x_str = NULL;
+    const char *y_str = NULL;
+    for (int i = 0; attr[i] != NULL; i += 2) {
+        const char *name = attr[i];
+        const char *value = attr[i+1];
+        if (strcmp(name, "x") == 0) {
+            x_str = value;
+        } else if (strcmp(name, "y") == 0) {
+            y_str = value;
+        }
+    }
+
+    if (x_str == NULL || y_str == NULL) {
+        fprintf(stderr, "error: x or y attribute missing from div element\n");
+        return -1;
+    }
+
+    int x = atoi(x_str);
+    int y = atoi(y_str);
+    LLVMValueRef quot = LLVMBuildSDiv(builder, x, y, "quot");
+    LLVMBuildRet(builder, quot);
+    return 0;
+}
+
+static int gen_div_node_end(tmpl_ctx_t ctx, const char *el)
+{
+    return 0;
+}
+
 static int init(tmpl_ctx_t data, tmpl_cb_t cb)
 {
-    cb->register_element_start_tag(data, "add", gen_add_node);
+    cb->register_element_start_tag(data, "add", gen_add_node_start);
+    cb->register_element_end_tag(data, "add", gen_add_node_end);
+    cb->register_element_start_tag(data, "sub", gen_sub_node_start);
+    cb->register_element_end_tag(data, "sub", gen_sub_node_end);
+    cb->register_element_start_tag(data, "mul", gen_mul_node_start);
+    cb->register_element_end_tag(data, "mul", gen_mul_node_end);
+    cb->register_element_start_tag(data, "div", gen_div_node_start);
+    cb->register_element_end_tag(data, "div", gen_div_node_end);
     mod = LLVMModuleCreateWithName("test_module");
     return 0;
 }
