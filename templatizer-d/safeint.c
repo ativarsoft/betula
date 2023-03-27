45798a78f99402a88824f5935b7f65d2031a0d1a
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-03-27 14:05:43 +0000

    Disabled integer overflow checking for non-gcc compilers and non-glibc standard libraries
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- templatizer-d/safeint.c
+++ templatizer-d/safeint.c
@@ -4,6 +4,7 @@
 
 typedef int tmpl_safeint_t;
 
+#if defined __GNUC__ && !defined __clang__ && defined __GLIBC__
 int tmpl_add(tmpl_safeint_t lhs, tmpl_safeint_t rhs, tmpl_safeint_t *res)
 {
     return __builtin_add_overflow(lhs, rhs, res);
@@ -18,3 +19,23 @@ int tmpl_mul(tmpl_safeint_t lhs, tmpl_safeint_t rhs, tmpl_safeint_t *res)
 {
     return __builtin_mul_overflow(lhs, rhs, res);
 }
+#else
+int tmpl_add(tmpl_safeint_t lhs, tmpl_safeint_t rhs, tmpl_safeint_t *res)
+{
+    *res = lhs + rhs;
+    return 0;
+}
+
+int tmpl_sub(tmpl_safeint_t lhs, tmpl_safeint_t rhs, tmpl_safeint_t *res)
+{
+    *res = lhs - rhs;
+    return 0;
+}
+
+int tmpl_mul(tmpl_safeint_t lhs, tmpl_safeint_t rhs, tmpl_safeint_t *res)
+{
+    *res = lhs * rhs;
+    return 0;
+}
+#endif
+
